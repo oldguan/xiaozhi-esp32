e02f3683f2cea46f0391274d6938bf47a3ecc0d1
oldguan <oldguan@gmail.com>
2026-07-27 09:00:04 +0800

    my-c3-7565完成

--- main/boards/my-c3-st7565/config.h
+++ main/boards/my-c3-st7565/config.h
@@ -5,16 +5,21 @@
 // 1. I2S 音频引脚 (麦克风与喇叭时钟引脚物理并联复用)
 #define AUDIO_INPUT_SAMPLE_RATE  16000
 #define AUDIO_OUTPUT_SAMPLE_RATE 16000
-#define AUDIO_I2S_GPIO_WS        GPIO_NUM_1 // LRCK
-#define AUDIO_I2S_GPIO_BCLK      GPIO_NUM_0 // BCLK
+// ==========================================
+// 核心时钟线 (功放与麦克风共用),并线，INMP441 和 MAX98357都需要接
+// ==========================================
+#define AUDIO_I2S_GPIO_WS        GPIO_NUM_1 // LRCK / WS
+#define AUDIO_I2S_GPIO_BCLK      GPIO_NUM_0 // BCLK / SCK
+
 #define AUDIO_I2S_GPIO_DIN       GPIO_NUM_5 // INMP441 SD
 #define AUDIO_I2S_GPIO_DOUT      GPIO_NUM_2 // MAX98357 DIN
+
 #define AUDIO_I2S_GPIO_MCLK      GPIO_NUM_NC // 没有MCLK则留空
 
 #define AUDIO_CODEC_PA_PIN       GPIO_NUM_NC
-#define AUDIO_CODEC_I2C_SDA_PIN  GPIO_NUM_8
-#define AUDIO_CODEC_I2C_SCL_PIN  GPIO_NUM_9
-#define AUDIO_CODEC_ES8311_ADDR  ES8311_CODEC_DEFAULT_ADDR
+//#define AUDIO_CODEC_I2C_SDA_PIN  GPIO_NUM_8
+//#define AUDIO_CODEC_I2C_SCL_PIN  GPIO_NUM_9
+//#define AUDIO_CODEC_ES8311_ADDR  ES8311_CODEC_DEFAULT_ADDR
 
 // ==========================================
 // Mini12864 (ST7565) 屏幕 SPI 引脚配置
@@ -29,7 +34,7 @@
 #define DISPLAY_HEIGHT           64
 
 // 3. 板载 LED 灯
-#define BUILTIN_LED_GPIO         GPIO_NUM_8
+//#define BUILTIN_LED_GPIO         GPIO_NUM_8
 #define LED_ACTIVE_LEVEL         0 // C3 Super Mini 是低电平点亮
 #endif
 
